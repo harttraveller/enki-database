278950acc1a4a667f9eba57885f60fd13cb46a9a
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-03 17:37:23 -0700

    enki/__meta__: add link

--- enki/__meta__.py
+++ enki/__meta__.py
@@ -3,3 +3,4 @@ from importlib import metadata
 __module__ = "enki"
 __package__ = __name__ = "enki-database"
 __version__ = metadata.version(__package__)
+__docs__ = "https://www.harttraveller.com/enki-database"
