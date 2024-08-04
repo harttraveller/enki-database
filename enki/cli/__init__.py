ce4625f47dabe2ab5d58ae887f84c3183cf44555
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-03 17:39:37 -0700

    enki/cli/__init__: update

--- enki/cli/__init__.py
+++ enki/cli/__init__.py
@@ -8,12 +8,13 @@ app = typer.Typer()
 
 
 @app.callback(name="enki", invoke_without_command=True)
-def callback():
-    cooltext(__module__)
-    print(
-        f"[deep_sky_blue1]Version:[/deep_sky_blue1] [turquoise2]{__version__}[/turquoise2]"
-    )
-    # todo: add printout for where cache and whether dumps/minidb dled
+def callback(ctx: typer.Context):
+    if ctx.invoked_subcommand is None:
+        cooltext(__module__)
+        print(
+            f"[deep_sky_blue1]Version:[/deep_sky_blue1] [turquoise2]{__version__}[/turquoise2]"
+        )
+        # todo: add printout for where cache and whether dumps/minidb dled
 
 
 @app.command()
