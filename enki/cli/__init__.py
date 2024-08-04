86776f4840f92a0145ca0fa6b7fee793b654be75
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-03 17:52:09 -0700

    enki/cli/__init__: update imports

--- enki/cli/__init__.py
+++ enki/cli/__init__.py
@@ -3,8 +3,10 @@ import webbrowser
 from rich import print
 from enki.__meta__ import __module__, __version__, __docs__
 from pycooltext import cooltext
+from . import cache
 
 app = typer.Typer()
+app.add_typer(cache.app)
 
 
 @app.callback(name=__module__, invoke_without_command=True)
