49be833b80134306b1f563b2bddeb0c09ac94e35
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-03 17:30:05 -0700

    enki/cli/__init__: .

--- enki/cli/__init__.py
+++ enki/cli/__init__.py
@@ -1,5 +1,13 @@
 import typer
 from rich import print
-from enki.__meta__ import __package__, __version__
+from enki.__meta__ import __package__, __module__, __version__
+from pycooltext import cooltext
 
 app = typer.Typer()
+
+
+@app.callback(name="enki", invoke_without_command=True)
+def callback():
+    cooltext(__module__)
+    print(f"[cyan]Version:[/cyan] [lightgreen]{__version__}[/lightgreen]")
+    # todo: add printout for where cache and whether dumps/minidb dled
