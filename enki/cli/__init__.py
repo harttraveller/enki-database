5ce820eb529fd863c88413d6b5616f328dbeb1fc
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-03 19:07:52 -0700

    enki/cli/__init__: update

--- enki/cli/__init__.py
+++ enki/cli/__init__.py
@@ -1,9 +1,10 @@
 import typer
 import webbrowser
 from rich import print
-from enki.__meta__ import __module__, __version__, __docs__
 from pycooltext import cooltext
-from . import cache
+from enki.__meta__ import __module__, __version__, __docs__
+from enki import loc
+from enki.cli import cache
 
 app = typer.Typer()
 app.add_typer(cache.app)
@@ -13,10 +14,12 @@ app.add_typer(cache.app)
 def callback(ctx: typer.Context):
     if ctx.invoked_subcommand is None:
         cooltext(__module__)
-        print(
-            f"[deep_sky_blue1]Version:[/deep_sky_blue1] [turquoise2]{__version__}[/turquoise2]"
-        )
-        # todo: add printout for where cache and whether dumps/minidb dled
+        metadata = f"[deep_sky_blue1]Version:[/deep_sky_blue1] [turquoise2]{__version__}[/turquoise2] | "
+        if loc.database.exists():
+            metadata += "[green]Local DB Available[/green]"
+        else:
+            metadata += "[red]Local DB Not Found[/red]"
+        print(metadata)
 
 
 @app.command()
