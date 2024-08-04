f4ca60ca6cb4c713e83ab4538d3067445e6d448d
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-03 17:56:06 -0700

    enki/cli/cache: update

--- enki/cli/cache.py
+++ enki/cli/cache.py
@@ -2,3 +2,19 @@ import typer
 
 
 app = typer.Typer()
+
+
+@app.command(name="vscode")
+def vscode():
+    """
+    Open cache dir in VSCode.
+    """
+    raise NotImplementedError()
+
+
+@app.command(name="delete")
+def delete():
+    """
+    Delete all locally cached data.
+    """
+    raise NotImplementedError()
